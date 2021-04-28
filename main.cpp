d9c92d2ece0087d5e168a9af5016f6e021154de7
vermelhachan790 <83366923+vermelhachan790@users.noreply.github.com>
2021-04-28 15:36:05 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main()
 {
 
 
-cout <<"Alerta: se você tem um celular com um processador ruim melhor nao roda o programa\n";
-cout <<"Aperte 1, para continuar\n";
+cout <<"Alerta: se você tem um celular com um processador ruim melhor nao roda o programa.";
+cout <<"Aperte 1, para continuar.";
 int pass;
 scanf ("%i" ,pass);
 if (pass == 1)
 {
-cout <<"Agora clique 2 para abrir a trava \n";
+cout <<"Agora clique 2 para abrir a trava.";
 }
 else{
 system("clear");
