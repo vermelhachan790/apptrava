55e98a7404ecfc62ba648eee537cd0347537bb3f
vermelhachan790 <83366923+vermelhachan790@users.noreply.github.com>
2021-04-28 15:32:03 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream.h>
+
+
+using namespace std;
+
+int main()
+{
+
+
+cout <<"Alerta: se você tem um celular com um processador ruim melhor nao roda o programa\n";
+cout <<"Aperte 1, para continuar\n";
+int pass;
+scanf ("%i" ,pass);
+if (pass == 1)
+{
+cout <<"Agora clique 2 para abrir a trava \n";
+}
+else{
+system("clear");
+}
+
+
+
+return 0;
+
+}
